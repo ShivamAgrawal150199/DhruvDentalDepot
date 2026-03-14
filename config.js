f9c0ed6ab478c51763690bdfc669b1ffd81ef6bb
ShivamAgrawal150199 <shivam.agrawal1501@gmail.com>
2026-03-14 13:38:35 +0530

    made changes for using local backend when runing locally

--- config.js
+++ config.js
@@ -3,6 +3,11 @@ window.APP_CONFIG = {
   //   (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   //     ? "http://localhost:4000"
   //     : "http://192.168.0.108:4000",
-  API_BASE: "https://dhruvdentaldepotbackend-production.up.railway.app",
+  API_BASE:
+    location.hostname === "localhost"
+      ? "http://localhost:4000"
+      : location.hostname === "127.0.0.1"
+        ? "http://127.0.0.1:4000"
+        : "https://dhruvdentaldepotbackend-production.up.railway.app",
   SITE_URL: "https://ddent.co.in"
 };
